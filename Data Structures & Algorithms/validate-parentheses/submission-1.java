e9ea86e2eb418450dbd2f05f06d7ce5f2f2819ce
Muskan Kumari <muskankumari7371039@gmail.com>
2026-08-15 13:33:48 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> st = new ArrayDeque<>();
+
+        for(int i = 0;i < s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()) return false;
+
+                char top = st.peek();
+                if((top == '(' && ch == ')') ||
+                (top == '{' && ch == '}') || 
+                (top == '[' && ch == ']')){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
